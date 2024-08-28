6b47b44f964c95c83700745f8f20d8b626c3ffce
reyhannaufalh <reyhannaufal19@gmail.com>
2024-08-28 09:54:40 +0700

    learn: Controller, Request, Response, Cookie, View

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as mustache from 'mustache-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser('secret'));
+  app.set('views', __dirname + '/../views');
+  app.set('view engine', 'html');
+  app.engine('html', mustache());
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/users')
+export class UserController {
+  @Get()
+  get(): string {
+    return 'GET /users';
+  }
+
+  @Get('/hello')
+  async getByQuery(@Query('name') name: string): Promise<string> {
+    return `Name is ${name}`;
+  }
+
+  @Get('/user/:id')
+  getById(@Param('id') id: string): string {
+    return `GET /users/${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST /user';
+  }
+
+  @Get('/response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'response',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/response',
+      statusCode: 301,
+    };
+  }
+
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('Cookie set');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name'];
+  }
+
+  @Get('/view/hello')
+  viewHello(@Query('name') name: string, @Res() response: Response) {
+    response.render('index.html', {
+      title: 'Views',
+      name: name,
+    });
+  }
+}
